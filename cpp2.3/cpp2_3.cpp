7b009c0b59167618d5ea13af49f183250c165893
cQula <doktorsad@yandex.ru>
2022-11-16 21:27:37 +0300

    correct

--- cpp2.3/cpp2_3.cpp
+++ cpp2.3/cpp2_3.cpp
@@ -2,21 +2,12 @@
 #include <string>
 
 struct address_storage{
-    std::string city;
-    std::string street;
-    int house;
-    int apartament;
-    int zip_code;
+    std::string city = "None";
+    std::string street = "None";
+    int house = 0;
+    int apartament = 0;
+    int zip_code = 0;
 };
-
-void designe_struct(const std::string &city, const std::string &street,
-const int &house, const int &apartament, const int &zip_code, address_storage &test){
-    test.city = city;
-    test.street = street;
-    test.house = house;
-    test.apartament = apartament;
-    test.zip_code = zip_code;
-}
 
 void print_address_storage(const address_storage &test){
     std::cout << "City: " << test.city << "\nStreet: " << test.street << "\nHouse: " 
@@ -24,10 +15,8 @@ void print_address_storage(const address_storage &test){
 }
 
 int main(){
-    address_storage test_first;
-    address_storage test_second;
-    designe_struct("Moscow", "Sofiyskaya", 37, 1, 236-123, test_first);
-    designe_struct("Piter", "Moscovskaya", 23, 2, 234-654, test_second);
+    address_storage test_first = {"Moscow", "Sofiyskaya", 37, 1, 236123};
+    address_storage test_second = {"Piter", "Moscovskaya", 23, 2, 234654};
     print_address_storage(test_first);
     print_address_storage(test_second);
     return 0;
